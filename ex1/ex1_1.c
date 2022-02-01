61ac83b33f743a6ce348c4e48559cf268f80e940
Kamilla Sulebakk <kamillasulebakk@gmail.com>
2022-02-01 14:07:00 +0100

    Ex 1 1 done

--- ex1/ex1_1.c
+++ ex1/ex1_1.c
@@ -1,10 +1,28 @@
-#include <stdlib.h>
+#include <stdio.h>
+/*
+#include <math.h>
 
 int main(int argc, char const *argv[]) {
-  float sum = 0;
-  for (int i=0; i<1000; i+=2)
-    sum = sum + 1/pow(2,i);
+  float sum = 1;
+  for (int i=2; i<1000; i+=4)
+    sum = sum - 1/pow(2,i);
+  for (int j=4; j<1000; j+=4)
+    sum = sum + 1/pow(2,j);
+
   printf("%f\n", sum);
-  
+
+  return 0;
+}
+*/
+int main(int argc, char const *argv[]) {
+  float sum = 0;
+  float add = 1;
+  for (int i=0; i<500; i++) {
+    sum += add;
+    add *= -0.25;
+  }
+
+  printf("%f\n", sum);
+
   return 0;
 }
